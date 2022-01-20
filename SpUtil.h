0bed089b803d76bdd2e6d13cbf59b992d97c9ee5
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-01-20 11:23:46 +0900

    impl input CRS

---
+++ SpUtil.h
@@ -0,0 +1,152 @@
+/* Copyright (C) 2021 T.Hishinuma, All rights reserved.*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#define MM_BANNER		"%%MatrixMarket"
+#define MM_FMT			"coordinate"
+#define MM_TYPE_REAL		"real"
+#define MM_TYPE_GENERAL		"general"
+
+void SpUtil_read_mm_header(FILE* file, int* N, int* nnz){
+
+    char buf[256],banner[128];
+    char mtx[64], fmt[64], dtype[64], dstruct[64];
+    char *p;
+    int	row, col, nz;
+
+    if( file==NULL )
+    {
+        printf("SpUtil Error: can not open file\n");
+        abort();
+    }
+    if( fgets(buf, 256, file) == NULL )
+    {
+        printf("SpUtil Error: blank file\n");
+        abort();
+    }
+
+    //check banner
+    rewind(file);
+    char* tmp = fgets(buf, 256, file);
+    if(tmp == NULL){abort();}
+    sscanf(buf, "%s %s %s %s %s", banner, mtx, fmt, dtype, dstruct);
+
+    for(p=mtx;*p!='\0';p++)     *p = (char)tolower(*p);
+    for(p=fmt;*p!='\0';p++)     *p = (char)tolower(*p);
+    for(p=dtype;*p!='\0';p++)   *p = (char)tolower(*p);
+    for(p=dstruct;*p!='\0';p++) *p = (char)tolower(*p);
+
+    if( strncmp(banner, MM_BANNER, strlen(MM_BANNER)) != 0)
+    {
+        printf("SpUtil Error: Not Matrix Market banner, banner is %s\n",banner);
+        abort();
+    }
+    if( strncmp(fmt, MM_FMT, strlen(MM_FMT))!=0 )
+    {
+        printf("SpUtil Error: Not Coodinate format\n");
+        abort();
+    }
+    if( strncmp(dtype, MM_TYPE_REAL, strlen(MM_TYPE_REAL))!=0 )
+    {
+        printf("SpUtil Error: Not real\n");
+        abort();
+    }
+    if( strncmp(dstruct, MM_TYPE_GENERAL, strlen(MM_TYPE_GENERAL))!=0 )
+    {
+        printf("SpUtil Error: Not general\n");
+        abort();
+    }
+    do
+    {
+        if( fgets(buf, 256, file) == NULL )
+        {
+            printf("SpUtil Error: check size error\n");
+            abort();
+        }
+    }while( buf[0]=='%' );
+
+    //check size
+    if( sscanf(buf, "%d %d %d", &col, &row, &nz ) != 3 )
+    {
+        printf("SpUtil Error: matrix size in unknown\n");
+        abort();
+        if(col != row){
+            printf("SpUtil Error: matrix is not square matrix (row!=col)\n");
+            abort();
+        }
+    }
+    *N = row;
+    *nnz = nz;
+}
+
+void SpUtil_mm2coo(FILE *file, int N, int nnz, int* row_index, int* col_index, double* val){
+
+    char	buf[256];
+    int	i;
+    int idx, jdx;
+    double value;
+    //rewind(file);
+
+    /* read data */
+
+    for(i=0;i<nnz;i++)
+    {
+        if( fgets(buf, 256, file) == NULL )
+        {
+            printf("SpUtil Error: cant read data, [row col value]\n"); abort();
+        }
+        if( sscanf(buf,"%d %d %lf",&idx, &jdx, &value) != 3 )
+        {
+            printf("SpUtil Error: not data, [col=%d,row=%d,val=%f]\n",idx,jdx,value); abort();
+        }
+        row_index[i] = idx-1;
+        col_index[i] = jdx-1;
+        val[i] = value;
+    }
+}
+
+void SpUtil_print_coo(int N, int nnz, int* row_index, int* col_index, double* val){
+    for(int i=0; i<nnz; i++){
+        printf("%d\t", row_index[i]);
+        printf("%d\t", col_index[i]);
+        printf("%e\n", val[i]);
+    }
+}
+
+void SpUtil_coo2crs(int N, int nnz, 
+        const int* coo_row, const int* coo_col, const double* coo_val,
+        int* row_ptr, int* col_ind, double* val){
+    int	i;
+    int count=0, jb = 0;
+    double value;
+
+    row_ptr[0] = 0;
+
+    for(i=0;i<nnz;i++)
+    {
+        int jdx = coo_col[i];
+        value = coo_val[i];
+
+        if(jb != jdx){
+            row_ptr[jdx+1] = i;
+        }
+        jb = jdx;
+        count++;
+
+        col_ind[i] = jdx;
+        val[i] = value;
+    }
+    row_ptr[N] = nnz;
+}
+
+void SpUtil_print_crs(int N, int nnz, int* row_ptr, int* col_ind, double* val){
+
+    for(int i=0; i<N; i++) {
+        for(int j = row_ptr[i]; j < row_ptr[i+1]; j++) {
+            printf("%d\t", i);
+            printf("%d\t", col_ind[j]);
+            printf("%e\n", val[j]);
+        }
+    }
+}
--- bicgstab.c
+++ bicgstab.c
@@ -1,7 +1,68 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<omp.h>
+#include "./SpUtil.h"
+#include "./myblas.h"
+
+struct CRS_matrix{
+    int N;
+    int nnz;
+    int* row_ptr;
+    int* col_ind;
+    double* val;
+};
 
 int main(int argc, char **argv){
 
+    // e.g. ./a.out data/test.mtx
+    if(argc!=2){
+        printf("error! $1 is matrix market file name\n");
+        abort();
+    }
+
+////////////////////
+// Create CRS format matrix from file
+///////////////////
+    int N, nnz;
+    //file open and read header to get matrix size
+    FILE* fp = fopen(argv[1], "r");
+    SpUtil_read_mm_header(fp, &N, &nnz);
+    //printf("N = %d, nnz = %d\n", N, nnz);
+    
+    // allocate COO array
+    int* coo_row_index = (int*)malloc(sizeof(int)*nnz);
+    int* coo_col_index = (int*)malloc(sizeof(int)*nnz);
+    double* coo_val = (double*)malloc(sizeof(double)*nnz);
+
+    // create COO from file
+    SpUtil_mm2coo(fp, N, nnz, coo_row_index, coo_col_index, coo_val);
+
+    // close
+    fclose(fp);
+
+    // allocate CRS array
+    struct CRS_matrix A;
+    A.row_ptr = (int*)malloc(sizeof(int)*(N+1));
+    A.col_ind = (int*)malloc(sizeof(int)*nnz);
+    A.val = (double*)malloc(sizeof(double)*nnz);
+    A.N = N;
+    A.nnz = nnz;
+    
+    // crate CRS from COO
+    SpUtil_coo2crs(N,nnz,
+                coo_row_index, coo_col_index, coo_val,
+                A.row_ptr, A.col_ind, A.val);
+
+    // debug
+    SpUtil_print_crs(N, nnz, A.row_ptr, A.col_ind, A.val);
+
+////////////////////
+// BiCGSTAB
+///////////////////
+    size_t maxiter = A.N
+    for(size_t iter = 0; iter < maxiter; iter++){
+        //impl..//
+
+    }
     return 0;
 }
